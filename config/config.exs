de6811df6eac6941248891de28d4730a0f5ba55b
Ian <bontaq@gmail.com>
2025-06-14 14:37:10 -0400

    Add accounts

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :moesey, :scopes,
+  user: [
+    default: true,
+    module: Moesey.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Moesey.AccountsFixtures,
+    test_login_helper: :register_and_log_in_user
+  ]
+
 config :moesey,
   ecto_repos: [Moesey.Repo],
   generators: [timestamp_type: :utc_datetime]
